a3a47534617f6cb573b9c9d2f9e84d43a65e01d8
valenaimale <valeaimale@gmail.com>
2026-05-28 16:10:42 -0300

    tablas migration sin commitear

--- db/migrations/20260506232402_primeras_tablas_migrations.php
+++ db/migrations/20260506232402_primeras_tablas_migrations.php
@@ -19,6 +19,18 @@ final class PrimerasTablasMigrations extends AbstractMigration
      */
     public function change(): void
     {
+        $tableGimnasio = $this->table('gimnasio');
+        $tableGimnasio->addColumn('nombre', 'string', '['limit'=>60]');//addColumn recibe como primer parametro 
+        //el nombre de la columna, el tipo de datos como segundo parametro y un tercer parametro opcional de una posible opcion
+        //si no se especifica nada, la columna es not null
+        $tableGimnasio->addColumn('mail','string', '['limit'=>40]');
+        $tableGimnasio->addColumn('contrasenia','string', '['limit'=>40]');
+        $tableGimnasio->addColumn('direccion','string', '['limit'=>40]');
+        $tableGimnasio->addColumn('mail','string', '['limit'=>40]');
+
+
+
+
 
     }
 }
