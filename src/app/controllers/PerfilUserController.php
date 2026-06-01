fd6fa38302ef1af3265f033abe583f9d3f3f2f16
valenaimale <valeaimale@gmail.com>
2026-06-01 15:45:00 -0300

    perfil de usuario

--- src/app/controllers/PerfilUserController.php
+++ src/app/controllers/PerfilUserController.php
@@ -15,18 +15,37 @@ class PerfilUserController
         $this->api = new ApiClient();
     }
 
-    public function mostrar()
+    /*public function mostrar()
     {
+        
         if (empty($_SESSION['user'])) {
             header('Location: /inicio-sesion');
             exit;
         }
-
         $userId   = $_SESSION['user']['id'];
         $response = $this->api->get("/usuarios/{$userId}", $_SESSION['jwt']);
 
         $userData = $response['ok'] ? $response['data'] : $_SESSION['user'];
 
         require $this->viewsDir . 'perfilUser.view.php';
-    }
+    }*/
+    //funcion mostrar generica de valen (se borra tanto controlador de perfil de gimnasio como controlador de perfil de gimnasio como perfio de entrenador)    
+    public function mostrar(){
+        $rol= $_SESSION['user']['rol'];
+        switch ($rol) {
+            case 'ALUMNO':
+                require $this->viewsDir . 'perfilUser.view.php';
+                break;
+            case 'ENTRENADOR':
+                require $this->viewsDir . 'perfilEntrenador.view.php';
+                break;
+            case 'GIMNASIO':
+                require $this->viewsDir . 'perfilGimnasio.view.php';
+                break;    
+            default:
+                header('Location: /');
+                exit;
+        }
+
+    }    
 }
