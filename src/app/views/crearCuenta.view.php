ff09a544ed60030ad908d4182cadc9b89df5f4c9
valenaimale <valeaimale@gmail.com>
2026-06-03 19:21:39 -0300

    Hay que arreglar las altas!!

--- src/app/views/crearCuenta.view.php
+++ src/app/views/crearCuenta.view.php
@@ -16,20 +16,21 @@
         <?php if (!empty($error)): ?>
             <p style="color:var(--rojo); margin-bottom:1rem; font-weight:600;"><?= htmlspecialchars($error) ?></p>
         <?php endif; ?>
-        <form action="/crearCuenta" method="post">
+        <form action="/crearCuenta" method="post" novalidate>
             <fieldset>
                 <legend>Datos personales:</legend>
                 <label for="nombre_apellido">Nombre y apellido</label>
-                <input type="text" id="nombre_apellido" name="nombre_apellido" placeholder="ej.: Maria Perez" required>
+                <input type="text" id="nombre_apellido" name="nombre_apellido" placeholder="Ej.: Maria Perez">
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="ej.:tuemail@email.com" required>
+                <input type="email" id="email" name="email" placeholder="Ej.:tuemail@email.com">
                 <label for="telefono">Telefono (opcional)</label>
-                <input type="tel" id="telefono" name="telefono" placeholder="ej.:+54 011 11111111">
+                <input type="tel" id="telefono" name="telefono" placeholder="Ej.:11 11111111">
+                <label for="fotoperfil">Foto de perfil (opcional)</label>
+                <input type="file" id="fotoperfil" name="fotoperfil" accept="image/jpeg, image/png, image/webp">
                 <label for="contraseña">Contraseña</label>
-                <input type="password" id="contraseña" name="contraseña" placeholder="ej.:tucontraseña" required>
+                <input type="password" id="contraseña" name="contraseña">
                 <label for="ccontraseña">Confirmar contraseña</label>
-                <input type="password" id="ccontraseña" name="ccontraseña" placeholder="ej.:tucontraseña" required>
-
+                <input type="password" id="ccontraseña" name="ccontraseña">
                 <fieldset>
                     <legend>Tipo de cuenta</legend>
                     <label>
