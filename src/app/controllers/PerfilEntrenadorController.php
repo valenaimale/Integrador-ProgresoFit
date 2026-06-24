547ec5ef0bbadfedcdaef2facc54938382ec3ea2
Ulises Casal <83846014+UlisesCasal@users.noreply.github.com>
2026-06-24 17:13:43 -0300

    Arreglado de vistas

--- src/app/controllers/PerfilEntrenadorController.php
+++ src/app/controllers/PerfilEntrenadorController.php
@@ -3,16 +3,35 @@
 namespace PAW\app\controllers;
 
 use PAW\Core\Controller;
+use PAW\app\services\ApiClient;
 
 class PerfilEntrenadorController extends Controller
 {
+    private ApiClient $api;
+
     public function __construct()
     {
         parent::__construct();
+        $this->api = new ApiClient();
     }
 
     public function mostrar()
     {
-        $this->render('perfilEntrenador.html.twig');
+        if (empty($_SESSION['user'])) {
+            header('Location: /inicio-sesion');
+            exit;
+        }
+        $userId  = $_SESSION['user']['id'];
+        $jwt     = $_SESSION['jwt'];
+
+        $response = $this->api->get("/usuarios/{$userId}", $jwt);
+        $userData = $response['ok'] ? $response['data'] : $_SESSION['user'];
+
+        $entResponse = $this->api->get("/entrenadores/{$userId}", $jwt);
+        if ($entResponse['ok']) {
+            $userData = array_merge($userData, $entResponse['data']);
+        }
+
+        $this->render('perfilEntrenador.html.twig', ['userData' => $userData]);
     }
 }
